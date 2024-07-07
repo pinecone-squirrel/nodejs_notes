70aa723849963e7e0990942ddbad8af3e08f2160
yuanmogul <yuanmogul@qq.com>
2024-07-07 23:13:53 +0800

    ✨ feat: 学习path模块

--- 3.path路径.mjs
+++ 3.path路径.mjs
@@ -13,7 +13,7 @@
 // 这是 Windows 系统的历史原因所致，早期的 Windows 操作系统采用了不同的设计选择。
 
 
-import { win32, basename, dirname, extname, join, resolve } from 'node:path'
+import { win32, basename, dirname, extname, join, resolve, posix, parse, format } from 'node:path'
 // windows与posix 差异
 
 // path.basename() 方法返回 path 的最后一部分
@@ -37,7 +37,35 @@ console.log(extname('.js')) // ''
 // 支持./  ../操作
 console.log(join('aaa', '/bbb', 'ccc', '/ddd/eee', '..'))
 // aaa\bbb\ccc\ddd(因为是在windows平台，所以是反斜杠)
-// 反复读这句话   使用特定于平台的分隔符作为定界符
+console.log(posix.join('aaa', '/bbb', 'ccc', '/ddd/eee', '..'))
+// aaa/bbb/ccc/ddd 这样就是正斜杠
+// 反复理解这句话   使用特定于平台的分隔符作为定界符
 
 // path.resolve() 方法将路径或路径片段的序列解析为绝对路径
-console.log(resolve('/aaa', '/bbb'))
+// 零长度的path将会被忽略
+// 不传path将会返回当前工作目录
+console.log(resolve()) // E:\cuboWork\nodejs_notes
+console.log(resolve('/aaa', '/bbb')) // /bbb
+// 给定路径是从右往左处理的，每个path会被追加到前面，直到构建绝对路径
+// 一旦构建出绝对路径，剩下多余的路径将会被抛弃掉，例如
+console.log(resolve('/aaa', '/bbb', 'ccc')) // /bbb/ccc
+// 因为ccc不是绝对路径，但是/bbb/ccc已经构建出了绝对路径，那么/aaa就被抛弃了
+// 如果处理完所有的path还没有构建出绝对路径，则使用当前工作目录作为
+console.log(resolve('aaa', 'bbb')) // E:\cuboWork\nodejs_notes\aaa\bbb
+
+
+// path.parse和path.format正好相反
+// path.parse() 方法接收一个路径字符串作为参数，返回一个包含路径各个组成部分的对象
+console.log(parse('/aaa/bbb/ccc/index.js'))
+// 返回对象具有以下属性
+const pathObject = {
+  root: '/', // 路径的根目录
+  dir: '/aaa/bbb/ccc', // 文件所在的目录
+  base: 'index.js', // 文件名
+  ext: '.js', // 文件扩展名
+  name: 'index' // 文件去除扩展名
+}
+
+// path.format() 方法从对象返回路径字符串
+console.log(format(pathObject))
+// /aaa/bbb/ccc/index.js
